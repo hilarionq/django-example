954da9b298af53e62434f3a143c11c3a536ad208
hilarionq <hilarionq@gmail.com>
2017-07-19 18:03:41 -0400

    Refactoring polls.index view, adding get_list_or_404 shortcut.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from .models import Question
 
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    latest_question_list = get_list_or_404(Question)[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
