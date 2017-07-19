9aa17e6171c8769c61376e4b80697eb041020b22
hilarionq <hilarionq@gmail.com>
2017-07-19 18:22:40 -0400

    Refactoring templates.polls.detail.html  and templates.polls.index.html removing hardcoded URLS in templates.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
-    #ex: /polls/5ll
+    #ex: /polls/5
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     #ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
