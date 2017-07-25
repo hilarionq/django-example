8d6448b056ea5bfe1462d5c55ee8a455f5f59a1f
hilarionq <hilarionq@gmail.com>
2017-07-25 12:07:39 -0400

    Adding test cases in order to validate the Django views.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
 # Create your views here.
@@ -10,8 +11,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """ Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """ Return the last five published questions.
+        (not including those set to be published in the future)"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -31,5 +33,3 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-        
